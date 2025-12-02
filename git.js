c07d0e89907477f2975a2013809a614dd3f29ebf
manasseh Anyankor <manassehanyankor@gmail.com>
2025-12-02 03:32:00 -0700

    Version14

--- git.js
+++ git.js
@@ -2,4 +2,4 @@ console.log('Hello world')
 console.log('Hello world2')
 
 console.log('Hello world2, im coming to let the ball roll')
-console.log('Hello world2, im going to make it big')
+console.log('Hello world2, im going to make it bigg')
